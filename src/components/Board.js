cff3e5c2b0cfa7b64380bcc266bdf94047e0943d
Angela <angela.ohh@gmail.com>
2019-06-20 14:14:22 -0700

    display list of hard coded cards on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import emoji from 'emoji-dictionary'
 
 import './Board.css';
 import Card from './Card';
@@ -8,18 +9,30 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       cards: [],
     };
   }
 
+  cardDisplay = CARD_DATA.cards.map((card, i) => {
+    if (card.Emoji) {
+      card.emoji = card.Emoji
+    }
+    return (
+      <Card 
+      key={i}
+      cardText={card.text}
+      cardEmoji={card.emoji} />
+    )
+  })
+
   render() {
     return (
       <div>
-        Board
+        {this.cardDisplay}
       </div>
     )
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 
+
 import './Card.css';
 
 class Card extends Component {
+
+  displayEmoji = (emojiInput) => {
+    if (emojiInput) {
+      return (
+        emoji.getUnicode(emojiInput)
+      )
+    } else {
+      return (null) 
+    }
+  }
+
   render() {
     return (
       <div className="card">
-        Card
+        <p> {this.props.cardText} </p>
+        <p> {this.displayEmoji(this.props.cardEmoji)} </p>
+        {/* <p> {emoji.unicode} </p> */}
       </div>
     )
   }
