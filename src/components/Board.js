5a62776a950446c0645482e07f6d7079c17b37a5
Ariana Bray <aribray@gmail.com>
2019-06-21 13:16:02 -0700

    added my board to list of all boards, added functionality to retrieve card data

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,13 +7,40 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+//Modify the Board component to use axios to retrieve card data from the end point, using the board endpoint you configured in the setup requirements.
+
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       cards: [],
+      cardList: [],
+      currentCard: undefined,
     };
+  }
+
+  componentDidMount() {
+      axios.get(`https://inspiration-board.herokuapp.com/boards/${this.props.boardName}/cards`)
+      .then((response) => {
+        console.log(response.data);
+
+        const cardList = response.data.map((data) => {
+          const newCard = {
+            id: data.card.id,
+            text: data.card.text,
+            emoji: data.card.emoji
+          }
+          return newCard;
+        })
+  
+        console.log(cardList);
+  
+        this.setState({ cardList });
+      })
+      .catch((error) => {
+        this.setState({ error: error.message })
+      })
   }
 
   render() {
