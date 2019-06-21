36b29ac98cb559618ae0d38d2a6b22db4b3f6e4e
Jansen <v.jansen.martin@gmail.com>
2019-06-21 15:47:27 -0700

    Add event handler to form submit button; Add preventDefault method

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -7,31 +7,33 @@ const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_
 
 class NewCardForm extends Component {
 
+    onSubmitCardButtonClick = (event) => {
+        event.preventDefault();
+        console.log("YOOOOOO, I GOT YOUR CARD");
+    }
+
     render() {
   
         return (
           <div className="new-card-form">
            <h3 className="new-card-form__header">Submit a New Card</h3>
            <form className="new-card-form__form"> 
+
                <div>
-               <textarea className="new-card-form__form-textarea" placeholder="Enter Stuff Here">
-            {/* //   name="firstAdjective"
-            //   value={firstAdjective}
-            //   onChange={this.onInputChange} */}
-              </textarea>
-               </div>
-               <div>
-               <input
-            //   name="firstAdjective"
-            //   value={firstAdjective}
-            //   onChange={this.onInputChange}
-              placeholder="emoji"
-              type="text" />
+                <textarea className="new-card-form__form-textarea" placeholder="Enter Stuff Here">
+                </textarea>
                </div>
 
                <div>
-                <input className="new-card-form__form-button" type="submit" value="Submit Card"/>
+                <input
+                  placeholder="emoji"
+                  type="text" />
+               </div>
+
+               <div>
+                <input onClick={this.onSubmitCardButtonClick} className="new-card-form__form-button" type="submit" value="Submit Card"/>
               </div>
+
            </form>
           </div>
         )
