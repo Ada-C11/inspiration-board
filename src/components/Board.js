cdb456b095221323bd092d1d8f57377a8649b7f8
Carla Bosco <carlabosco@gmail.com>
2019-06-20 19:35:16 -0700

    Initial setup for api call

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,13 +7,24 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+const API_URL = 'https://inspiration-board.herokuapp.com/boards';
+
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       cards: [],
     };
+  }
+
+  componentDidMount() {
+    axios.get(API_URL)
+    .then ((response) => {
+      const cards = response.data.map((card) => {
+        
+      })
+    })
   }
 
   render() {
