b5230b6ee3416b3c68b0da4bc0964d43bd0485c9
Tatiana Quintana <tatsqui@gmail.com>
2019-06-21 14:52:59 -0700

    debugged delete request

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,7 +12,8 @@ class Board extends Component {
     super(props);
     this.state = {
       cards: [],
-      myUrl: props.url + props.boardName + '/cards'
+      myUrl: props.url + props.boardName + '/cards',
+      message: null
     };
     
   }
@@ -29,32 +30,52 @@ class Board extends Component {
   }
 
 
-  removeCard = (cardIndex) => {
-    console.log('in remove card')
-    // const newState = this.state;
-    // const allCards = newState.cards;
-    // const deletedCard = allCards.splice(cardIndex, 1);
-    // newState = allCards;
+  removeCard = (cardID) => {
+    const newState = this.state.cards;
+    const deletedCard = newState.splice(cardID, 1);
+    this.setState(newState);
 
-    // this.setState(newState);
+    const deletedID = deletedCard[0].card.id
+
+    axios.delete(this.props.deleteURL + deletedID)
+      .then( response => {
+       alert(`Successfully deleted pet with ${deletedID}`)
+       this.setState({ message: `Successfully deleted pet with ${deletedID}`}) // will use to add info for user
+      })
+      .catch( error => {
+        console.log(error.message)
+        alert(`Encountered an error: ${error.message}`)
+      });
+
   }
+
+  reportStatus = (message) => {
+    console.log('inside error handling helper')
+    return (message) => {
+      return ( <ul>
+        <li>
+          message
+        </li>
+      </ul> )
+    }
+  }
+
+  
 
 
   render() {
-    const generatedCards = this.state.cards.map( card => {
-     return( 
-     <li>
-       <Card 
-         card={ card.card } 
-         deleteCardCallback={this.removeCard}
-       />
-     </li>);
-    });
-    return (
-      <div>
-        {generatedCards}
-      </div>
-    )
+    
+    const generatedCards = this.state.cards.map((card, i ) => {
+     return( <li key={card.card.id}>
+              <Card 
+                card={ card.card }
+                cardIndex={i} 
+                deleteCardCallback={this.removeCard}
+              />
+              </li>);
+            });
+
+    return ( <div> {generatedCards} </div>);
   }
 
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,25 +5,18 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
-  constructor(props){
-    super(props)
-    console.log(props);
-    this.state = {
-      card: props.card
-    }
-  }
 
-  handleButtonClick = (e) => {
+  handleButtonClick = () => {
     // pass along cardIndex to delete card and remove from API...
-    console.log(this.state.card.id)
+    this.props.deleteCardCallback(this.props.cardIndex)
   }
 
   render() {
     return (
       <div className="card">
-        <h3>{this.state.id}</h3>
-        <h4>{this.state.card.text}</h4>
-        <h6>{this.state.card.emoji ? emoji.getUnicode(this.state.card.emoji) : null}</h6>
+        <h3>{this.props.id}</h3>
+        <h4>{this.props.card.text}</h4>
+        <h6>{this.props.card.emoji ? emoji.getUnicode(this.props.card.emoji) : null}</h6>
         <button onClick={this.handleButtonClick}>Delete</button>
       </div>
     )
