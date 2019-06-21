24d8601e78fc383f0cd7429f529f4ed9bfe19d5a
Shirley Chu <shirley.cchu@gmail.com>
2019-06-21 11:09:39 -0700

    got emojis to show up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import emoji from 'emoji-dictionary';
+import emoji, { getUnicode } from 'emoji-dictionary';
 
 import './Card.css';
 
 class Card extends Component {
   render() {
+    let cardEmoji = this.props.emoji; 
+    cardEmoji = cardEmoji ? getUnicode(cardEmoji) : cardEmoji
+    
     return (
       <div className="card">
         {/* Card */}
         <p className = 'card__content-text'> {this.props.text} </p>
-        <p className = 'card__content-emoji'> {this.props.emoji} </p>
+        <p className = 'card__content-emoji'> {cardEmoji} </p>
       </div>
     )
   }
