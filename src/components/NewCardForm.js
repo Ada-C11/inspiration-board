53beed980c3f4317d14a6ac37cd5f00e1997b716
Cyndi Lopez <cyndilopez6@gmail.com>
2019-06-21 11:29:54 -0700

    text input and smiley drop down list added to form

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -5,7 +5,7 @@ import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
 
-class NewCardForm extends Components {
+class NewCardForm extends Component {
     constructor(props) {
         super(props)
     
@@ -14,12 +14,32 @@ class NewCardForm extends Components {
         emoji: ""
         }
     }
+ 
+    generateSelectList = () => {
+        return EMOJI_LIST.map((smiley) => {
+            return (
+                
+                <option value={smiley}>{emoji.getUnicode(smiley)}</option>
+            )
+        })
+        
+    }
 
     render() {
         return(
             <div>
                 <h1>Add a New Card</h1>
+                <label htmlFor="name">Text</label>
+                <input name="name" 
+                type="text" 
+                onChange = {this.onNameChange} 
+                value={this.state.name}/>
+                <select value="select emoji">
+                    {this.generateSelectList()}
+                </select>
             </div>
         )
     }
 }
+
+export default NewCardForm;
