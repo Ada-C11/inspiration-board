c0912effb353bceb3443e7eda0f9297ebbccf78c
Carla Bosco <carlabosco@gmail.com>
2019-06-21 20:59:36 -0700

    Can display emojis

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,21 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
+const emojis = emoji
+
 class Card extends Component {
   render() {
+    const {id, text, emoji} = this.props.card
+    
     return (
-      <div className="card">
-        Card
-      </div>
+      <section className="card">
+        <h4>{text}</h4>
+
+        {/* If there is an emoji, display it */}
+        { emoji &&
+          <div className='card__content-emoji'>{emojis.getUnicode(emoji)}</div>
+        }
+      </section>
     )
   }
 }
