8076b1daad5111f2c9122519e180c31f516b91c4
Amy Phung <phung.amy85@gmail.com>
2019-06-22 15:42:57 -0700

    add test for Card

---
+++ src/components/Card.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Card from './Card';
+
+
+describe('Card', () => {
+  test('that it matches an existing snapshot', () => {
+    const wrapper = shallow(
+      <Card
+        key={85}
+        id={85}
+        text={"tesxt"}
+        emoji={"heart_eyes"}
+        deleteCard={() => { }}
+      />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
