08c96d31d6978539efa6b8ef775a359b7ec4546d
Shamira <marshallscm@gmail.com>
2019-06-22 15:45:41 -0700

    added cardcallback function

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,21 +13,69 @@ class Board extends Component {
 
     this.state = {
       cards: [],
+      errorMessage: null,
+
     };
   }
+  
+  componentDidMount() {
+    const url = (this.props.url);
+    const name = (this.props.boardName);
+    const myURL = url+name+"/cards"
+    axios.get(myURL)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          cards: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  onDeleteCard =(cardID) => {
+
+  }
+
+  addCardCallback = (cardInfo) => {
+    const cardIds = this.state.cards;
+
+    axios.post(myURL, card)
+      .then((response) => {
+        let updatedCard = this.state.cards;
+        updatedCard.push(cardInfo);
+        this.setState({cards: updatedCard});
+
+      })
+      .catch((errors) => {
+        this.setState({ error: error.message })
+    });
+  }
+
 
   render() {
+    const cards = this.state.cards.map((card, i) => {
+      return <Card
+                key={i}
+                id={card["card"].id}
+                text={text["card"].text}
+                emoji={emoji["card"].emoji} />
+    })
     return (
-      <div>
-        Board
-      </div>
+      <section>
+        <div className="board">
+          {cards}
+          Board
+        </div>
+      </section>
     )
   }
 
 }
 
 Board.propTypes = {
-
+  boardName: PropTypes.string
 };
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,28 @@ import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 
 import './Card.css';
+import Axios from 'axios';
 
 class Card extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: props.id, 
+      text: props.text, 
+      emoji: props.emoji ? props.emoji : '',
+    }
+  }
+
   render() {
+    
     return (
       <div className="card">
+        <section className="card__content">
+          <p className="card__content-text">{this.state.text}</p>
+          <p className="card__content-text">{emoji.getUnicode(this.state.emoji)}</p>
+        </section>
         Card
       </div>
     )
@@ -15,6 +32,9 @@ class Card extends Component {
 }
 
 Card.propTypes = {
+  id: PropTypes.number,
+  text: PropTypes.string,
+  emoji: PropTypes.string,
 
 };
 
