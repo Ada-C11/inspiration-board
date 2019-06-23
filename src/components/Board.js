1e6bfc260ad9fcb61c7479f20bd990d95632fced
Jansen <v.jansen.martin@gmail.com>
2019-06-23 13:02:50 -0700

    Add POST request for new card

--- src/components/Board.js
+++ src/components/Board.js
@@ -57,6 +57,19 @@ class Board extends Component {
 
   addCard = (cardInfo) => {
     console.log(cardInfo);
+    // let updatedCards = [...this.state.cards];
+    // updatedCards.push(cardInfo);
+    // this.setState({cards: updatedCards});
+
+    axios.post("https://inspiration-board.herokuapp.com/boards/jansen-martin/cards", cardInfo)
+      .then((response) => {
+        console.log(response);
+      //What should we do when we know the post request worked?
+      })
+      .catch((error) => {
+        console.log(error);
+      // What should we do when we know the post request failed?
+      })
   }
 
 
