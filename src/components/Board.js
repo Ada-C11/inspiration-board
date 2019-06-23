fa0ab52de12ec45f71827bfa3b064ae12edbd5e8
Jansen <v.jansen.martin@gmail.com>
2019-06-23 13:43:00 -0700

    Add index prop to card

--- src/components/Board.js
+++ src/components/Board.js
@@ -23,12 +23,12 @@ class Board extends Component {
 
     console.log("Inside componentDidMount");
 
-    const URL = this.props.url + this.props.boardName + "/cards";
+    const getURL = this.props.url + this.props.boardName + "/cards";
 
     // console.log("This is the URL");
     // console.log(this.props.url);
 
-    axios.get(URL)
+    axios.get(getURL)
       .then((response) => {
       console.log(response.data);
 
@@ -49,22 +49,24 @@ class Board extends Component {
         this.setState({ cards: cardList });
 
       })
-      // })
       .catch((error) => {
-      console.log(error);
+        this.setState({ error: error.message });
       });
   }
 
   addCard = (cardInfo) => {
     console.log(cardInfo);
-    // let updatedCards = [...this.state.cards];
-    // updatedCards.push(cardInfo);
-    // this.setState({cards: updatedCards});
 
-    axios.post("https://inspiration-board.herokuapp.com/boards/jansen-martin/cards", cardInfo)
+    const postURL = this.props.url + this.props.boardName + "/cards";
+
+    axios.post(postURL, cardInfo)
       .then((response) => {
-        console.log(response);
+        console.log(response.data.id);
       //What should we do when we know the post request worked?
+        let updatedCards = [...this.state.cards];
+        updatedCards.push(cardInfo);
+        this.setState({cards: updatedCards});
+        
       })
       .catch((error) => {
         console.log(error);
@@ -75,38 +77,38 @@ class Board extends Component {
 
   deleteCard = (id) => {
     const deleteUrl = "https://inspiration-board.herokuapp.com/cards/" + id;
-    // console.log(id);
 
     axios.delete(deleteUrl)
       .then((response) => {
-      console.log(response);
 
       let updatedCards = [...this.state.cards];
 
-     const index = updatedCards.findIndex((card) => card.id == id);
+      const index = updatedCards.findIndex((card) => card.id == id);
 
-     updatedCards.splice(index, 1);
+      updatedCards.splice(index, 1);
 
       this.setState({
-          cards: updatedCards,
+        cards: updatedCards,
       });
-
 
       })
       .catch((error) => {
-      console.log(error);
+        console.log(error);
       })
   };
 
 
   render() {
-    // console.log(this.state.cards);
-    // console.log("Hello")
     const cards = this.state.cards.map((card, i) => {
 
       return (
-        <li key={card.id}>
-          <Card id={card.id} quote={card.text} emoji={card.emoji} deleteCardCallback={this.deleteCard} />
+        <li key={i}>
+          <Card 
+            id={card.id}
+            index={i}
+            quote={card.text} 
+            emoji={card.emoji} 
+            deleteCardCallback={this.deleteCard} />
         </li>
       );
     });
