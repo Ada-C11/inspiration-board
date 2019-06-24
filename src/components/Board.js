ac18d46d3ca97106ce2a89f7b5a1e3f96d4ce62e
Jansen <v.jansen.martin@gmail.com>
2019-06-24 04:59:06 -0700

    Remove comments and console.log functions

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,16 +19,12 @@ class Board extends Component {
   }
 
   componentDidMount() {
-
-    console.log("Inside componentDidMount");
-
     const { url, boardName } = this.props;
 
     const getURL = url + boardName + "/cards";
 
     axios.get(getURL)
       .then((response) => {
-      console.log(response.data);
 
         const cardList = response.data.map((card) => {
     
@@ -42,8 +38,6 @@ class Board extends Component {
           return newCard;
         })
 
-        console.log(cardList);
-
         this.setState({ cards: cardList });
 
       })
@@ -53,34 +47,28 @@ class Board extends Component {
   }
 
   addCard = (cardInfo) => {
-    console.log(cardInfo);
 
     const { url, boardName } = this.props;
     const postURL = url + boardName + "/cards";
 
     axios.post(postURL, cardInfo)
       .then((response) => {
-        console.log(response.data.card.id);
         cardInfo.id = response.data.card.id;
-      //What should we do when we know the post request worked?
         let updatedCards = [...this.state.cards];
         updatedCards.push(cardInfo);
         this.setState({cards: updatedCards});
         
       })
       .catch((error) => {
-        console.log(error);
         this.setState({ error: `${error.message} while adding your card!` })
       })
   }
 
 
   deleteCard = (index) => {
-    console.log(index);
 
     let updatedCards = this.state.cards;
     let id = updatedCards[index].id;
-    console.log(id);
 
     const deleteUrl = "https://inspiration-board.herokuapp.com/cards/" + id;
 
@@ -95,7 +83,6 @@ class Board extends Component {
 
       })
       .catch((error) => {
-        console.log(error);
         this.setState({ error: `${error.message} while deleting your card!`})
       })
   };
