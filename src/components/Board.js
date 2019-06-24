d4317d064af7d177310a3deb2c858980ac8570a3
Mina Shin <minamishin@hotmail.com>
2019-06-23 21:49:38 -0700

    added emojis and text to notes

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,26 +8,32 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       cards: [],
     };
   }
+ 
 
   render() {
-    return (
-      <div>
-        Board
-      </div>
-    )
-  }
+    let cards = this.state.cards.map((card, i) => {
+      return (
+          <Card key={ i } id={card.card.id} text={card.card.text} emoji={card.card.emoji} />
+      )
+    })
 
+  return (
+    <div className="board">
+      {cards};
+    </div>
+  )};
 }
 
 Board.propTypes = {
-
+  url: PropTypes.string,
+  boardName: PropTypes.string,
 };
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,17 +5,26 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+  addEmoji = () => {
+    const { noteEmoji } = this.props
+
+    return noteEmoji;
+  }
   render() {
     return (
       <div className="card">
-        Card
+        <div className="card__content">
+          <span className="card__content-text">{ this.noteText }</span>
+          <span className="card__content-emoji">{ this.addEmoji() }</span>
+        </div>
       </div>
     )
   }
 }
 
 Card.propTypes = {
-
+  noteText: PropTypes.string,
+  noteEmoji: PropTypes.string,
 };
 
 export default Card;
