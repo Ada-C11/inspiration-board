6cbfe9a154ed293f6d90b7a5b40e7b587bb46fe5
Jessica <jessica.homet@gmail.com>
2019-06-24 09:31:37 -0700

    cards load from API

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,21 +16,66 @@ class Board extends Component {
     };
   }
 
-  cardComponents = CARD_DATA.cards.map((card, i) => {
-    card.text = card.text ? card.text : "";
-    card.emoji = card.emoji ? card.emoji : "octopus";
-    return (
-      <Card 
-        key={i}
-        text={card.text}
-        emoji={card.emoji} />
-    )
-  })
+  componentDidMount() {
+    axios.get('https://inspiration-board.herokuapp.com/boards/jessica/cards')
+    .then((response) => {
+      console.log("In .then!");
+
+      const cardsFM = response.data.flatMap(card => {
+        // if (card.name && card.breed && card.about) {
+          return [{
+            ...card
+            // species: pet.breed.toLowerCase()
+          }];
+        // } else {
+        //   return [];
+        // }
+      });
+
+      this.setState({ cards: cardsFM });
+
+      console.log(this.state.cards)
+
+    })
+    .catch((error) => {
+      console.log("In .catch!");
+
+      this.setState({
+        errorMessage: error.message
+      })
+    })
+  }
+
+  // cardComponents = CARD_DATA.cards.map((card, i) => {
+  //   // console.log(this)
+  //   card.text = card.text ? card.text : "";
+  //   card.emoji = card.emoji ? card.emoji : "octopus";
+  //   return (
+  //     <Card 
+  //       key={i}
+  //       text={card.text}
+  //       emoji={card.emoji} />
+  //   )
+  // })
+
+  cardComponents = () => {
+    const components = this.state.cards.map((card, i) => {
+      card.card.text = card.card.text ? card.card.text : "";
+      card.card.emoji = card.card.emoji ? card.card.emoji : "octopus";
+      return (
+        <Card 
+          key={i}
+          text={card.card.text}
+          emoji={card.card.emoji} />
+      )
+    })
+    return components
+  } 
 
   render() {
     return (
       <div>
-        {this.cardComponents}
+        {this.cardComponents()}
       </div>
     )
   }
