c3dd0952ea93666444f37881ea4c355a148b33d1
laneia <laneiamurray@gmail.com>
2019-06-25 08:07:17 -0700

    added axios functionality for laneia board

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,10 +16,31 @@ class Board extends Component {
     };
   }
 
+  componentDidMount(){
+
+     axios.get(`${this.props.url}${this.props.boardName}`+"/cards")
+     .then((response)=>{
+       const cards = response.data.map((cardObj)=>{
+         const newCard = {
+           ...cardObj.card,
+         };
+         return newCard;
+       }).filter((card, index)=> index < 5);
+       this.setState({
+         cards:cards,
+       });
+     })
+     .catch((error)=>{
+       this.setState({
+         errorMessage: error.message,
+       });
+     });
+  }
+
   render() {
-    const cardList = CARD_DATA.cards.map((card, i)=>{
+    const cardList = this.state.cards.map((card)=>{
       return <Card
-        key={i}
+        key={card.id}
         text={card.text}
         emoji={card.emoji}
       />
