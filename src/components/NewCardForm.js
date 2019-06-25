db9fff7f8a80699e2fd232f179f3e35881789896
Jessica <jessica.homet@gmail.com>
2019-06-24 21:00:40 -0700

    refactor and style

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import emoji from 'emoji-dictionary';
 import axios from 'axios';
 import './NewCardForm.css';
-import { createDecipher } from 'crypto';
-
+import emoji from 'emoji-dictionary';
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
 
 class NewCardForm extends Component {
@@ -16,34 +14,23 @@ class NewCardForm extends Component {
       emoji: "",
     };
 
-    this.state = {...this.cleared}
+    this.state = { ...this.cleared };
   }
 
   addCard = (event) => {
-
     event.preventDefault();
+
     const card = this.state;
 
     axios.post('https://inspiration-board.herokuapp.com/boards/jessica/cards', {text: card.text, emoji: card.emoji})
     .then((response) => {
-
-      console.log(card)
       this.props.addCardCallback(card);
 
       this.setState({...this.cleared});
     })
     .catch((error) => {
-      console.log("In .catch!");
-      const card = this.state;
-
-      console.log(card)
-
-      this.setState({
-        errorMessage: error.message
-      })
+      this.setState({ errorMessage: error.message });
     })
-
-    
   }
 
   onInputChange = (event) => {
@@ -57,41 +44,46 @@ class NewCardForm extends Component {
   }
 
   render() {
+    const emojiOptions = EMOJI_LIST.map((thisEmoji, i) => {
+      return (
+        <option value={ thisEmoji } key={ i }>
+          { emoji.getUnicode(thisEmoji) }
+        </option>
+      );
+    });
+
     return (
-      <form  className="new-card-form" onSubmit={this.addCard}>
-        <h3>Add a Card</h3>
-        <label>
-          Text: 
+      <div className="new-card-form">
+        <h3 className="new-card-form__header">Add a Card</h3>
+        <form className="new-card-form__form" onSubmit={ this.addCard }>
+          <label className="new-card-form__form-label">Text:</label>
           <input 
+            className="new-card-form__form-textarea"
             name="text" 
             type="text"
-            value={this.state.text}
-            onChange={this.onInputChange}></input>
-        </label>
-        <label>
-          Emoji:
+            value={ this.state.text }
+            onChange={ this.onInputChange }></input>
+          <label className="new-card-form__form-label">Emoji:</label>
           <select 
+            className="new-card-form__form-select"
             name="emoji"  
-            value={this.state.emoji}
-            onChange={this.onInputChange}>
-              <option value="">Please Select an Emoji</option>
-              <option value="heart_eyes">Heart Eyes</option>
-              <option value="cat">Cat</option>
-              <option value="dog">Dog</option>
-              <option value="beer">Beer</option>
-              <option value="clap">Clap</option>
-              <option value="sparkling_heart">Sparkling Heart</option>
-              <option value="heart_eyes_cat">Heart Eyes Cat</option>
+            value={ this.state.emoji }
+            onChange={ this.onInputChange }>
+              { emojiOptions }
           </select>
-        </label>
-        <input className="btn btn-success new-card-form--submit" type="submit" name="submit" value="Add a Card" />
-      </form>
-    )
+          <input
+            className="new-card-form__form-button"
+            type="submit"
+            name="submit"
+            value="Add a Card" />
+        </form>
+      </div>
+    );
   }
 }
 
 NewCardForm.propTypes = {
-
+  addCardCallback: PropTypes.func.isRequired,
 };
 
 export default NewCardForm;
