ca2cd767b5de180697b0440b78d5c71a49aadaac
Jessica <jessica.homet@gmail.com>
2019-06-24 20:22:54 -0700

    convert card from classical to functional component

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,33 +5,28 @@ import axios from 'axios';
 
 import './Card.css';
 
-class Card extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Card = (props) => {
 
-  // 
-
-  onClickDelete = () => {
+  const onClickDelete = () => {
     this.props.deleteCardCallback(this.props.id);
-  }
+  } 
 
-  render() {
-    return (
-      //.card__delete somewhere
-      <div className="card">
-        <p onClick={this.onClickDelete} className='card__delete'>x</p>
-        <div className='card__content'>
-          <p className='card__content-text'>{this.props.text}</p>
-          <p className='card__content-emoji'>{emoji.getUnicode(this.props.emoji)}</p>
-        </div>
+  return (
+    //.card__delete somewhere
+    <div className="card">
+      <p onClick={ onClickDelete } className='card__delete'>x</p>
+      <div className='card__content'>
+        <p className='card__content-text'>{props.text}</p>
+        <p className='card__content-emoji'>{emoji.getUnicode(props.emoji)}</p>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Card.propTypes = {
-
+  emoji: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  deleteCardCallback: PropTypes.func.isRequired
 };
 
 export default Card;
